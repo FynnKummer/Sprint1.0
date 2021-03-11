9968aea535eccde16d9a3c47b19d8dae86ce82cd
Malteklm <malte.klarmann@gmail.com>
2021-03-11 12:04:16 +0100

    Hallo

---
+++ Malte/Malte/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie alt ist Sie?: ");
+            int alter = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sie ist: " + alter);
+
+            if (alter >= 18)
+            {
+                Console.WriteLine("Gönn dia Bruda");
+            }
+            else
+            {
+                Console.WriteLine("Bruda, du krigst Anseige");
+            }
+            Console.ReadLine();
+        }
+    }
+}
