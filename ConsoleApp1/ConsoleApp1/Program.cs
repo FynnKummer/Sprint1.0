3c8c1acb0921fe1f659d194bf784ef578588e23c
Jonatan200 <80037037+Jonatan200@users.noreply.github.com>
2021-03-11 12:12:02 +0100

    Console Test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Test Jonathan 
-            // Test Lars
+            Console.WriteLine("Test");
+            Console.ReadKey();
         }
 
     }
