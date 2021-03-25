aa255dfcc6eed8e330c5796f6d6daa23a9b10b1a
Jonatan200 <80037037+Jonatan200@users.noreply.github.com>
2021-03-25 10:06:41 +0100

    Update Program.cs

--- Malte/Malte/Program.cs
+++ Malte/Malte/Program.cs
@@ -17,7 +17,7 @@ namespace Impfstoff
             //Abfrage Zahl
 
 
-            if (Impfstoff == 0)
+            if (Impfstoff == 2)
 
             {
                 Console.WriteLine("Keinen mehr?");
