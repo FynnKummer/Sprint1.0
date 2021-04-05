75ec2170770195ebdf85a3760d3305a86bd3a87a
LarsBohling1 <lars.bohling@student.jade-hs.de>
2021-04-05 16:15:42 +0200

    Update Program.cs

--- Sprint1/Sprint1/Program.cs
+++ Sprint1/Sprint1/Program.cs
@@ -328,7 +328,7 @@ namespace Sprint1
         public static double masse()
         {
             //hier den Code reinschreiben
-            //Fehlermeldung sind normal und bleiben bis return xy; ergänzt wird
+            //Fehlermeldung sind normal und bleiben bis return xy; ergänzt wird g
 
             return 1; 
         }
