104ddfc3c0f47560fb957ee647cdc4a47dc66176
Jonatan200 <80037037+Jonatan200@users.noreply.github.com>
2021-04-29 12:07:55 +0200

    .

---
+++ Test Jonathan/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public void EmailSenden(string text)
+
+        {
+            string betreff = "Anfrage";
+            string server = "";
+            int port = 25;
+            string user = "max.Musterman";
+            string passwort = "123";
+
+            MailMessage Mail = new MailMessage();
+
+            //Absender
+            Mail.From = new MailAddress(//Sender);
+
+            //Empfänger 
+            Mail.To.Add(//Empfänger);
+
+            // Betreff
+
+            Mail.Subject = (betreff);
+
+            Mail.Body = text;
+
+            // Abseneserver 
+            SmtpClient mailClient = new SmtpClient(server,port);
+
+            mailClient.Credentials = new System.Net.NetworkCredential(user,passwort);
+
+            mailClient.Send(Mail);
+
+
+        }   
+
+    }
+}
