fcf15dcaad7954125bc5a9e22dde8476c8684ece
Jonatan200 <80037037+Jonatan200@users.noreply.github.com>
2021-04-29 17:07:40 +0200

    Update Program.cs

--- Test Jonathan/ConsoleApp1/ConsoleApp1/Program.cs
+++ Test Jonathan/ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,43 +9,61 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
+             emailsenden("Test 2");    
+
+            new ExcelControll();
+            {
+                Microsoft.Offi
+            }
         }
 
-        public void EmailSenden(string text)
+         public static void emailsenden( string text)
 
         {
+            
             string betreff = "Anfrage";
-            string server = "";
-            int port = 25;
-            string user = "max.Musterman";
-            string passwort = "123";
+            string server = "mail.gmx.net";
+            int port = 587;
+            string user = "456263856";
+            string passwort = "1Q2w3e4r5t6z7u8_";
 
             MailMessage Mail = new MailMessage();
 
             //Absender
-            Mail.From = new MailAddress(//Sender);
+            Mail.From = new MailAddress("hsp.anfragen@gmx.net");
 
             //Empfänger 
-            Mail.To.Add(//Empfänger);
+            Mail.To.Add("jonathan.rietig@aol.de");
 
             // Betreff
 
-            Mail.Subject = (betreff);
+            Mail.Subject = betreff;
 
             Mail.Body = text;
 
+          
+
             // Abseneserver 
-            SmtpClient mailClient = new SmtpClient(server,port);
-
-            mailClient.Credentials = new System.Net.NetworkCredential(user,passwort);
-
+            SmtpClient mailClient = new SmtpClient(server);
+            
+            mailClient.UseDefaultCredentials = false;
+            mailClient.EnableSsl = true;
+            mailClient.Port = port;
+            mailClient.Credentials = new System.Net.NetworkCredential(user, passwort);
+            
             mailClient.Send(Mail);
 
+            Console.WriteLine("Fertig");
+            Console.ReadKey();
 
-        }   
+        }
 
+    }
+
+    class ExcelControll
+    {
+        
     }
 }
