a398aebc8f7fc103b436a329fe595fb2842761d6
Jonathan20 <80037037+Jonatan200@users.noreply.github.com>
2021-04-30 17:53:35 +0200

    Update Program1.cs

--- Test Jonathan/ConsoleApp1/ConsoleApp1/Program1.cs
+++ Test Jonathan/ConsoleApp1/ConsoleApp1/Program1.cs
@@ -8,22 +8,71 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ConsoleApp1
 {
-    class ExcelControll
+
+    class Programm
     {
         static void Main()
+        {
+            ExcelControll.Excel_erstellen();
+        }
+    }
+
+    class ExcelControll
+    {
+        public static void Excel_erstellen()
         {
             new ExcelControll();
         }
 
         ExcelControll()
         {
+            int anzahl = 4;
             Excel.Application excelApp = new Excel.Application();
             excelApp.Visible = true;
             excelApp.Workbooks.Add();
 
             Excel._Worksheet mySheet = (Excel.Worksheet)excelApp.ActiveSheet;
 
-            mySheet.Cells[1, "A"] = "Hallo Welt";
+            // Kategorien festlegen
+
+            mySheet.Cells[2,1] = "Techniche Details";
+            mySheet.Cells[3,1] = "Schraubenlänge";
+            mySheet.Cells[4,1] = "Schlüsselweite";
+            mySheet.Cells[5,1] = "Gewindedurchmesser";
+            mySheet.Cells[6,1] = "Masse pro Stück";
+            mySheet.Cells[7,1] = "Gesamtgewicht";
+            mySheet.Cells[8,1] = "Steigung";
+            mySheet.Cells[9,1] = "Gewindetiefe";
+            mySheet.Cells[10,1] = "Rundung";
+            mySheet.Cells[11,1] = "Flankendurchmesser";
+            mySheet.Cells[12,1] = "Flankenwinkel";
+            mySheet.Cells[13,1] = "";
+            mySheet.Cells[14,1] = "Elastizitätzgrenze";
+            mySheet.Cells[15,1] = "Zugfestigkeit";
+            mySheet.Cells[16,1] = "";
+            mySheet.Cells[17,1] = "Preis (Netto)";
+            mySheet.Cells[18,1] = "Summe";
+            mySheet.Cells[19,1] = "Stückpreis";
+            mySheet.Cells[20,1] = "";
+            mySheet.Cells[21,1] = "Preis (Brutto)";
+            mySheet.Cells[22,1] = "Summe";
+            mySheet.Cells[23,1] = "Stückpreis";
+
+           // Design festlegen 
+            mySheet.Range["A1","E25"].AutoFormat(Excel.XlRangeAutoFormat.xlRangeAutoFormatList2);
+
+            for (int i=1; i<= anzahl; i++)
+            {
+                mySheet.Cells[1, 1+i] = "Schraube " +i+"    ";
+            }
+            
+
+            for (int i = 1; i<9; i++)
+            {
+                mySheet.Columns[i].AutoFit();
+            }
+            
+
         }
 
          public static void emailsenden( string text)
