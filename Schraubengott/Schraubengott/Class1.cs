68ec95822d7513b0d0844ec00a6a76b14fc58814
FynnKummer <fynn.kummer@student.jade-hs.de>
2021-05-04 13:07:13 +0200

    1

--- Schraubengott/Schraubengott/Class1.cs
+++ Schraubengott/Schraubengott/Class1.cs
@@ -8,5 +8,14 @@ namespace Schraubengott
 {
     class Class1
     {
+        public static void Main()
+        {
+            Schraube[] feld = new Schraube[5];
+
+            for( int i = 0; i < feld.Length; i++)
+            {
+                feld[i] = new Schraube();
+            }
+        }
     }
 }
