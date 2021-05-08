085111fa592803583b0a0a53defbe93567535bb9
FynnKummer <fynn.kummer@student.jade-hs.de>
2021-05-08 12:07:22 +0200

    Revert "h"
    This reverts commit 905939ed9f7252fc732e75cab883a4cc5687aeb0.

--- Schraubengott/Schraubengott/MainWindow.xaml.cs
+++ Schraubengott/Schraubengott/MainWindow.xaml.cs
@@ -37,9 +37,10 @@ namespace Schraubengott
             {
                 feld[i] = new Schraube();         // Array wird mit Objekten gefüllt
             }
-
+<<<<<<< HEAD
             //cmb_nr.SelectedItem = screw1;
-
+=======
+>>>>>>> parent of 41512f3 (.)
 
             InitializeComponent();
 
@@ -182,7 +183,7 @@ namespace Schraubengott
             }
             //hier dann alle Werte in die Schraube speichern
 
-
+<<<<<<< HEAD
             
             feld[nr].festigkeit = cbfk.SelectedItem.ToString();
             if(txt_menge.Text.ToString()==null)
@@ -195,7 +196,8 @@ namespace Schraubengott
             feld[nr].gewinde_festlegen(cbgewinde.SelectedItem.ToString());
 
             lbl_test2.Content = feld[nr].gewinde;
-
+=======
+>>>>>>> parent of 41512f3 (.)
         }
 
         private void cbfk_SelectionChanged(object sender, SelectionChangedEventArgs e)
