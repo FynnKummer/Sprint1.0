e522d862e0c3b66f07e2cc2f8784294b50c7f463
Jonatan200 <80037037+Jonatan200@users.noreply.github.com>
2021-05-10 11:48:42 +0200

    .

--- Schraubengott/Schraubengott/MainWindow.xaml.cs
+++ Schraubengott/Schraubengott/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail;
+using Excel = Microsoft.Office.Interop.Excel;
 
 
 
@@ -28,9 +30,11 @@ public partial class MainWindow : Window
         Schraube[] feld = new Schraube[5];      //Array vom Typ Schraube erstellen
         int nr = 0;                             //Variable für den Index des Feldes Schraube
         int new_screw_int = 1;                  //Varibale für Neue Schraube Button und Combobox mit Schraubenauswahl
-        
+
         // hier noch ne Random Nummer erzeugen lassen für ne Bestell und Kudnennummer
 
+        Random nummer = new Random();
+        int bestellnummer;
 
         public MainWindow()
         {
@@ -42,6 +46,10 @@ public partial class MainWindow : Window
             
             InitializeComponent();
             cmb_nr.SelectedIndex = 0;           //Combobox hat von Anfang an die erste Schraube ausgewählt
+
+            // Bestellnummer 
+            bestellnummer = nummer.Next(10000000, 99999999);
+
 
         }
 
@@ -509,11 +517,193 @@ public partial class MainWindow : Window
 
         private void btnexcel_Click(object sender, RoutedEventArgs e)
         {
-            ExcelControll. Excel_erstellen(Schraube[] arr)
-            {
-                new ExcelControll(arr);
-            }
+            bool senden = false; 
+            ExcelControll.ExelContoll_aufrufen(feld, senden, bestellnummer);
+        }
+
+        private void btnangebot_Click(object sender, RoutedEventArgs e)
+        {
+            bool senden = true;
+            ExcelControll.ExelContoll_aufrufen(feld, senden, bestellnummer);
         }
     }
+
+    class ExcelControll
+    {
+        
+
+        public static void ExelContoll_aufrufen(Schraube[] arr, bool senden, int bestellnummer)
+        {
+            Excel_erstellen(arr, senden, bestellnummer);
+        }
+        public static void Excel_erstellen(Schraube[] arr, bool senden, int bestellnummer)
+        {
+            new ExcelControll(arr, senden, bestellnummer);
+        }
+
+        ExcelControll(Schraube[] arr, bool senden, int bestellnummer)
+        {
+            // Erstellen einer Neuen Exelmappe 
+            Excel.Application excelApp = new Excel.Application();
+           if (senden == true)
+            {
+                excelApp.Visible = false;
+            }
+           else
+            {
+                excelApp.Visible = true;
+                
+            }
+
+            excelApp.Workbooks.Add();
+
+            // Hinzufügen einer Seite? 
+            Excel._Worksheet mySheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+
+
+            // Kategorien festlegen
+
+            mySheet.Cells[2, 1] = "Techniche Details";
+            mySheet.Cells[3, 1] = "Schraubenlänge";
+            mySheet.Cells[4, 1] = "Gewindelänge";
+            mySheet.Cells[5, 1] = "Schlüsselweite";
+            mySheet.Cells[6, 1] = "Gewindedurchmesser";
+            mySheet.Cells[7, 1] = "Masse pro Stück";
+            mySheet.Cells[8, 1] = "Gesamtgewicht";
+            mySheet.Cells[9, 1] = "Gewindesteigung";
+            mySheet.Cells[10, 1] = "Gewindetiefe";
+            mySheet.Cells[11, 1] = "Rundung";
+            mySheet.Cells[12, 1] = "Flankendurchmesser";
+            mySheet.Cells[13, 1] = "Kerndurchmesser";
+            mySheet.Cells[14, 1] = "Flankenwinkel";
+            mySheet.Cells[15, 1] = "";
+            mySheet.Cells[16, 1] = "Elastizitätzgrenze";
+            mySheet.Cells[17, 1] = "Zugfestigkeit";
+            mySheet.Cells[18, 1] = "";
+            mySheet.Cells[19, 1] = "Preis (Netto)";
+            mySheet.Cells[20, 1] = "Summe";
+            mySheet.Cells[21, 1] = "Stückpreis";
+            mySheet.Cells[22, 1] = "";
+            mySheet.Cells[23, 1] = "Preis (Brutto)";
+            mySheet.Cells[24, 1] = "Summe";
+            mySheet.Cells[25, 1] = "Stückpreis";
+            mySheet.Cells[27, 1] = "Menge";
+
+
+            // Listenformat einführen 
+            mySheet.Range["A1", "E29"].AutoFormat(Excel.XlRangeAutoFormat.xlRangeAutoFormatList2);
+
+            // Werte der Schrauben in Tabelle eingeben 
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                mySheet.Cells[1, i + 2] = arr[i].name;
+                mySheet.Cells[3, i + 2] = arr[i].laenge + " mm";
+                mySheet.Cells[4, i + 2] = arr[i].gewindelaenge + " mm";
+                mySheet.Cells[5, i + 2] = arr[i].schluesselbreite + " mm";
+                mySheet.Cells[6, i + 2] = arr[i].gewinde;
+                mySheet.Cells[7, i + 2] = Math.Round(arr[i].masse, 2) + " g";
+                mySheet.Cells[8, i + 2] = Math.Round(arr[i].gesamtgewicht, 2) + " g";
+                mySheet.Cells[9, i + 2] = Math.Round(arr[i].gewindesteigung, 2) + " mm";
+                mySheet.Cells[10, i + 2] = Math.Round(arr[i].gewindetiefe, 2) + " mm";
+                mySheet.Cells[11, i + 2] = Math.Round(arr[i].gewinderundung, 2) + " mm";
+                mySheet.Cells[12, i + 2] = Math.Round(arr[i].flankendurchmesser, 2) + " mm";
+                mySheet.Cells[13, i + 2] = Math.Round(arr[i].kerndurchmesser, 2) + " mm";
+                mySheet.Cells[14, i + 2] = Math.Round(arr[i].flankenwinkel, 2) + "°";
+
+                mySheet.Cells[16, i + 2] = Math.Round(arr[i].elastizitätsgrenze, 2) + " N/mm²";
+                mySheet.Cells[17, i + 2] = Math.Round(arr[i].Zugfestigkeit, 2) + " N/mm²";
+
+
+                mySheet.Cells[20, i + 2] = Math.Round(arr[i].nettopreis_Summe, 2) + "€";
+                mySheet.Cells[21, i + 2] = Math.Round(arr[i].nettoeinzelpreis, 2) + "€";
+
+
+                mySheet.Cells[24, i + 2] = Math.Round(arr[i].preis_summe, 2) + "€";
+                mySheet.Cells[25, i + 2] = Math.Round(arr[i].stückpreis, 2) + "€";
+
+                mySheet.Cells[27, i + 2] = arr[i].menge;
+
+                mySheet.Cells[28, i + 2].AddComment("Anmerkung S " + i);
+
+            }
+
+
+            // Zellenbreite an Text anpassen 
+            for (int i = 1; i < 9; i++)
+            {
+                mySheet.Columns[i].AutoFit();
+            }
+
+
+            if (senden == true)
+            {
+                mySheet.SaveAs(@"C:\Windows\Temp\Bestellung " + Convert.ToString(bestellnummer) + ".xlsx");
+
+
+                excelApp.Workbooks.Close();
+
+                Emailsenden(bestellnummer);
+            }
+
+        
+
+            //Exel Speichern für Kunden 
+
+            /* string Pfad = @"C:\Desktop\";
+
+             mySheet.SaveAs(Pfad+@"\Bestellung " + Convert.ToString(bestellnummer) + ".xlsx");
+            */
+
+            // Excel muss noch geschlossen werden 
+
+          
+
+        }
+
+        public static void Emailsenden(int bestellnummer)
+
+        {
+            string text = "Anfrage";
+            string betreff = "Anfrage";
+            string server = "mail.gmx.net";
+            int port = 587;
+            string user = "456263856";
+            string passwort = "1Q2w3e4r5t6z7u8_";
+
+            MailMessage Mail = new MailMessage();
+
+            //Absender
+            Mail.From = new MailAddress("hsp.anfragen@gmx.net");
+
+            //Empfänger 
+            Mail.To.Add("hsp.anfragen@gmx.net");
+
+            // Betreff
+
+            Mail.Subject = betreff;
+
+            Mail.Body = text;
+
+            Attachment Tabelle = new Attachment(@"C:\Windows\Temp\Bestellung " + Convert.ToString(bestellnummer) + ".xlsx");
+
+            Mail.Attachments.Add(Tabelle);
+
+
+            // Abseneserver 
+            SmtpClient mailClient = new SmtpClient(server);
+
+            mailClient.UseDefaultCredentials = false;
+            mailClient.EnableSsl = true;
+            mailClient.Port = port;
+            mailClient.Credentials = new System.Net.NetworkCredential(user, passwort);
+
+            mailClient.Send(Mail);
+
+
+        }
+
     }
+}
 
